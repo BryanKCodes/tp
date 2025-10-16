45da841baf190c99bda61b81c0d768e1fe967f0d
Justin Wong <justinwongjianhui@gmail.com>
2025-10-16 18:44:23 +0800

    Make Rank comparable

--- src/main/java/seedu/address/model/person/Rank.java
+++ src/main/java/seedu/address/model/person/Rank.java
@@ -6,29 +6,36 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 /**
  * Represents a Person's rank in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidRank(String)}
+ * Implements Comparable to allow natural ordering from lowest (Iron) to highest (Challenger).
  */
-public class Rank {
+public class Rank implements Comparable<Rank> {
 
     private enum RankName {
-        IRON("iron"),
-        BRONZE("bronze"),
-        SILVER("silver"),
-        GOLD("gold"),
-        PLATINUM("platinum"),
-        EMERALD("emerald"),
-        DIAMOND("diamond"),
-        MASTER("master"),
-        GRANDMASTER("grandmaster"),
-        CHALLENGER("challenger");
+        IRON("iron", 0),
+        BRONZE("bronze", 1),
+        SILVER("silver", 2),
+        GOLD("gold", 3),
+        PLATINUM("platinum", 4),
+        EMERALD("emerald", 5),
+        DIAMOND("diamond", 6),
+        MASTER("master", 7),
+        GRANDMASTER("grandmaster", 8),
+        CHALLENGER("challenger", 9);
 
         private final String name;
+        private final int order;
 
-        RankName(String name) {
+        RankName(String name, int order) {
             this.name = name;
+            this.order = order;
         }
 
         public String getName() {
             return name;
+        }
+
+        public int getOrder() {
+            return order;
         }
     }
 
@@ -85,6 +92,33 @@ public class Rank {
     @Override
     public int hashCode() {
         return value.hashCode();
+    }
+
+    /**
+     * Compares this rank with another rank for order.
+     * Returns a negative integer, zero, or a positive integer as this rank is
+     * less than, equal to, or greater than the specified rank.
+     *
+     * @param other The rank to be compared.
+     * @return A negative integer, zero, or a positive integer as this rank is
+     *         less than, equal to, or greater than the specified rank.
+     */
+    @Override
+    public int compareTo(Rank other) {
+        return Integer.compare(getRankOrder(), other.getRankOrder());
+    }
+
+    /**
+     * Returns the ordinal value of this rank (0 for Iron, 9 for Challenger).
+     * Used for comparison purposes.
+     */
+    private int getRankOrder() {
+        for (RankName rankName : RankName.values()) {
+            if (rankName.getName().equalsIgnoreCase(this.value)) {
+                return rankName.getOrder();
+            }
+        }
+        return 0; // Default to lowest if not found
     }
 
 }
