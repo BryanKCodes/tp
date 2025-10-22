12d9d3e265e83becf989396f844c514140fc6faf
BryanKCodes <bryankoh1228@gmail.com>
2025-10-23 04:28:01 +0800

    Enable editing of players within teams with validation

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -8,6 +8,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_ROLE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -27,6 +28,9 @@ import seedu.address.model.person.Person;
 import seedu.address.model.person.Rank;
 import seedu.address.model.person.Role;
 import seedu.address.model.tag.Tag;
+import seedu.address.model.team.Team;
+import seedu.address.model.team.exceptions.DuplicateChampionException;
+import seedu.address.model.team.exceptions.DuplicateRoleException;
 
 /**
  * Edits the details of an existing person in the address book.
@@ -70,23 +74,34 @@ public class EditCommand extends Command {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        List<Person> lastShownList = model.getFilteredPersonList();
+        List<Person> lastShownPersonList = model.getFilteredPersonList();
+        List<Team> lastShownTeamList = model.getFilteredTeamList();
 
-        if (index.getZeroBased() >= lastShownList.size()) {
+        if (index.getZeroBased() >= lastShownPersonList.size()) {
             throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
         }
 
-        Person personToEdit = lastShownList.get(index.getZeroBased());
-
-        if (model.isPersonInAnyTeam(personToEdit)) {
-            throw new CommandException(Messages.MESSAGE_PERSON_IN_TEAM);
-        }
-
+        Person personToEdit = lastShownPersonList.get(index.getZeroBased());
         Person editedPerson = createEditedPerson(personToEdit, editPersonDescriptor);
 
         if (!personToEdit.isSamePerson(editedPerson) && model.hasPerson(editedPerson)) {
             throw new CommandException(MESSAGE_DUPLICATE_PERSON);
         }
+
+        Optional<Team> teamOptional = model.getFilteredTeamList().stream()
+                .filter(team -> team.hasPerson(personToEdit))
+                .findFirst();
+
+        if (teamOptional.isPresent()) {
+            Team teamToEdit = teamOptional.get();
+            try {
+                Team editedTeam = createEditedTeam(teamToEdit, personToEdit, editedPerson);
+                model.setTeam(teamToEdit, editedTeam);
+            } catch (DuplicateRoleException | DuplicateChampionException e) {
+                throw new CommandException(e.getMessage());
+            }
+        }
+
 
         model.setPerson(personToEdit, editedPerson);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
@@ -111,6 +126,19 @@ public class EditCommand extends Command {
 
         // Preserve role, rank, and champion from the original person
         return new Person(id, updatedName, updatedRole, updatedRank, updatedChampion, updatedTags, wins, losses);
+    }
+
+    private static Team createEditedTeam(Team teamToEdit, Person personToEdit, Person editedPerson) {
+        assert teamToEdit.hasPerson(personToEdit);
+
+        String id = teamToEdit.getId();
+        List<Person> updatedPersonList = new ArrayList<>(teamToEdit.getPersons());
+        int personIndex = updatedPersonList.indexOf(personToEdit);
+        updatedPersonList.set(personIndex, editedPerson);
+        int wins = personToEdit.getWins();
+        int losses = personToEdit.getLosses();
+
+        return new Team(id, updatedPersonList, wins, losses);
     }
 
     @Override
