3f7e6f8b0178d751845afe444af9c32857454e76
Nguyen Tuan Duong <tuanduong18001@gmail.com>
2025-10-26 00:30:19 +0800

    Add support for DeleteStats
    Add command to delete the latest performance record of a Person based on
    their Index in the UI.

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -12,6 +12,7 @@ import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.DeleteStatsCommand;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.ExportCommand;
@@ -116,6 +117,9 @@ public class AddressBookParser {
 
         case LoseCommand.COMMAND_WORD:
             return new LoseCommandParser().parse(arguments);
+
+        case DeleteStatsCommand.COMMAND_WORD:
+            return new DeleteStatsCommandParser().parse(arguments);
 
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
