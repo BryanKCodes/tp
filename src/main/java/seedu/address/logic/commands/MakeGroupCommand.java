045f25978c4d30126aa124d5f9e47f6157270cd0
BryanKCodes <bryankoh1228@gmail.com>
2025-10-29 23:56:52 +0800

    Update 'makeGroup' command to accept index arguments instead of names

--- src/main/java/seedu/address/logic/commands/MakeGroupCommand.java
+++ src/main/java/seedu/address/logic/commands/MakeGroupCommand.java
@@ -1,19 +1,15 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.team.Team;
 import seedu.address.model.team.exceptions.DuplicateChampionException;
@@ -21,94 +17,87 @@ import seedu.address.model.team.exceptions.DuplicateRoleException;
 import seedu.address.model.team.exceptions.InvalidTeamSizeException;
 
 /**
- * Creates a team consisting of 5 existing players, manually provided by the user
+ * Creates a team consisting of 5 existing persons, manually provided by the user
  */
 public class MakeGroupCommand extends Command {
 
-    public static final String COMMAND_WORD = "makegroup";
+    public static final String COMMAND_WORD = "makeGroup";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Creates a team of 5 players. "
-            + "All players must already exist in SummonersBook.\n"
-            + "Parameters: "
-            + PREFIX_NAME + "NAME1 "
-            + PREFIX_NAME + "NAME2 "
-            + PREFIX_NAME + "NAME3 "
-            + PREFIX_NAME + "NAME4 "
-            + PREFIX_NAME + "NAME5\n"
-            + "Example: " + COMMAND_WORD + " "
-            + PREFIX_NAME + "Alice "
-            + PREFIX_NAME + "Bob "
-            + PREFIX_NAME + "Cathy "
-            + PREFIX_NAME + "Derek "
-            + PREFIX_NAME + "Ella";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Creates a team of 5 persons identified by the index "
+            + "numbers used in the displayed person list.\n"
+            + "Parameters: INDEX_1 INDEX_2 INDEX_3 INDEX_4 INDEX_5\n"
+            + "Example: " + COMMAND_WORD + " 1 2 3 4 5";
 
-    public static final String MESSAGE_SUCCESS = "New team created: %1$s";
-    public static final String MESSAGE_INSUFFICIENT_PLAYERS = "Exactly 5 player names must be provided.";
-    public static final String MESSAGE_DUPLICATE_NAMES = "Duplicate player names found in the input.";
-    public static final String MESSAGE_PLAYER_NOT_FOUND = "Player '%1$s' does not exist in SummonersBook.";
-    public static final String MESSAGE_REUSED_PLAYERS = "Some players are already in other teams.";
+    public static final String MESSAGE_MAKE_GROUP_SUCCESS = "New team created: %1$s";
 
-    private final List<Name> playerNames;
+    public static final String MESSAGE_INVALID_TEAM_SIZE = "Exactly %1$d index numbers must be provided.";
+    public static final String MESSAGE_DUPLICATE_INDEX = "Duplicate index numbers found in the input.";
+    public static final String MESSAGE_REUSED_PERSON = "Person is already in another team.\n%1$s";
+
+    private final List<Index> indexList;
 
     /**
-     * Creates a MakeGroupCommand to create a team with the specified players.
+     * Creates a MakeGroupCommand to create a team with the specified persons.
      */
-    public MakeGroupCommand(List<Name> playerNames) {
-        requireNonNull(playerNames);
-        this.playerNames = playerNames;
+    public MakeGroupCommand(List<Index> indexList) {
+        requireNonNull(indexList);
+        this.indexList = indexList;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
 
-        // === 1. Check number of players ===
-        if (playerNames.size() != 5) {
-            throw new CommandException(MESSAGE_INSUFFICIENT_PLAYERS);
+        // Check number of indices
+        if (indexList.size() != Team.TEAM_SIZE) {
+            throw new CommandException(String.format(MESSAGE_INVALID_TEAM_SIZE, Team.TEAM_SIZE));
         }
 
-        // === 2. Check for duplicates in provided names ===
-        Set<Name> uniqueNames = new HashSet<>(playerNames);
-        if (uniqueNames.size() < playerNames.size()) {
-            throw new CommandException(MESSAGE_DUPLICATE_NAMES);
+        // Check for duplicates in provided index numbers
+        long uniqueIndexCount = indexList.stream().map(Index::getZeroBased).distinct().count();
+        if (uniqueIndexCount < indexList.size()) {
+            throw new CommandException(MESSAGE_DUPLICATE_INDEX);
         }
 
-        // === 3. Fetch and validate all players ===
-        Set<Person> teamMembers = new HashSet<>();
-        for (Name name : playerNames) {
-            Optional<Person> personOpt = model.findPersonByName(name);
-            if (personOpt.isEmpty()) {
-                throw new CommandException(String.format(MESSAGE_PLAYER_NOT_FOUND, name.fullName));
-            }
-            if (model.isPersonInAnyTeam(personOpt.get())) {
-                throw new CommandException(String.format(MESSAGE_REUSED_PLAYERS, name.fullName));
-            }
-            teamMembers.add(personOpt.get());
+        // Check if indices are valid
+        List<Person> lastShownList = model.getFilteredPersonList();
+        boolean hasInvalidIndex = indexList.stream().anyMatch(index -> index.getZeroBased() >= lastShownList.size());
+        if (hasInvalidIndex) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        // Fetch stream of persons from index
+        List<Person> teamMembers = indexList.stream().map(index -> lastShownList.get(index.getZeroBased())).toList();
+
+        // Check if persons are not in other teams
+        Optional<Person> reusedPersonOptional = teamMembers.stream().filter(model::isPersonInAnyTeam).findFirst();
+        if (reusedPersonOptional.isPresent()) {
+            throw new CommandException(String.format(MESSAGE_REUSED_PERSON,
+                    Messages.format(reusedPersonOptional.get())));
         }
 
         // Construct and check for duplicates
-        Team newTeam;
         try {
-            newTeam = new Team(new ArrayList<>(teamMembers)); // Team handles role/rank validation
+            // Team creation handles duplicate role, rank, and champion validations
+            Team teamToAdd = new Team(teamMembers);
+            model.addTeam(teamToAdd);
+            return new CommandResult(String.format(MESSAGE_MAKE_GROUP_SUCCESS, Messages.format(teamToAdd)));
         } catch (InvalidTeamSizeException | DuplicateRoleException | DuplicateChampionException e) {
             throw new CommandException(e.getMessage());
         }
-
-        model.addTeam(newTeam);
-        return new CommandResult(String.format(MESSAGE_SUCCESS, Messages.format(newTeam)));
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this
                 || (other instanceof MakeGroupCommand
-                && playerNames.equals(((MakeGroupCommand) other).playerNames));
+                && indexList.equals(((MakeGroupCommand) other).indexList));
     }
 
     @Override
     public String toString() {
         return new ToStringBuilder(this)
-                .add("playerNames", playerNames)
+                .add("indexList", indexList)
                 .toString();
     }
 }
